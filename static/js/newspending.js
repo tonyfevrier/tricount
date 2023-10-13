47611068a3e16dd9b165a3f1a4f5c491f8413371
tonyfevrier <tony.fevrier62@gmail.com>
2023-10-13 19:17:31 +0200

    Avancée dans le JS de newspending

--- static/js/newspending.js
+++ static/js/newspending.js
@@ -1,7 +1,4 @@
 /*
-input sur le titre : compteur sur le nb de lettres           ok
-montant rempli : calcul automatique de la part de chacune des personnes cochées. 
-clic sur checkbox de pour qui : coche ou décoche tous
 clic sur avancé : apparition de parts pour chaque participant et du mot simple à la place de "avancé"
     changement des parts : recalcul des montants de chaque personne cochée.
 clic sur simple : disparition des parts et apparition de avancé.
@@ -14,12 +11,18 @@ const amount = document.body.querySelector(".amount");
 let counter = document.body.querySelector(".counter");
 let checkboxDiv = document.body.querySelector(".list-receivers");  
 let receivers = document.body.querySelectorAll(".receiver");
+let toggleCheckbox = document.body.querySelector(".toggle-checkboxes");
 
 
 title.addEventListener("input", userWriting);
 title.addEventListener("keydown", userTaping);
 amount.addEventListener("input", userAmount);
 checkboxDiv.addEventListener("click",userChecking);
+toggleCheckbox.addEventListener("click",userToggle);
+
+
+let click = new Event("click", {bubbles: true,});
+
 
 
 function userWriting(event){ 
@@ -37,44 +40,72 @@ function userTaping(event){
 
 function userAmount(event){
     //Quand l'utilisateur écrit un montant, il est partagé sur les personnes cochées.
-    
-    let table = calculateIndividualAmountsAndStoreSpendingParticipants(event); 
-    attributeIndividualAmount(table[0],table[1]);
+    let table = calculateIndividualAmountsAndStoreSpendingParticipants(); 
+    attributeIndividualAmount(table[0],table[1],table[2]); 
 }
 
 
-function userChecking(event){
+function userChecking(event){ 
     //Clic sur une checkbox : le participant est décoché ou coché 
     if (event.target.type !== 'checkbox') return; 
     //si on coche quelqu'un, on recalcule la répartition des montants. 
-    userAmount(event);
+    userAmount(event); 
 
-    
-    /*if (event.target.checked){ 
-        console.log(1);
-    } else {
-        console.log(2);
-    }*/
+    //si on décoche un participant, on décoche le checkbox principal */
+    if (!event.target.checked) toggleCheckbox.checked = false;
 }
 
-function calculateIndividualAmountsAndStoreSpendingParticipants(event){
-    /*on compte le nb de personnes cochées et on les listes*/
+function userToggle(event){ 
+    /*dans le cas où au moins 1 n'était pas coché, tout le monde devient coché.*/
+    nb_checked = NumberOfCheckedPeople();
+    if (nb_checked < receivers.length){
+        for (let receiver of receivers){ 
+            receiver.checked = true;
+            receiver.dispatchEvent(click);
+        }
+    } else {
+        for (let receiver of receivers){ 
+            receiver.checked = false;
+            receiver.dispatchEvent(click);
+        }
+    }
+}
+
+function calculateIndividualAmountsAndStoreSpendingParticipants(){
+    /*on compte le nb de personnes cochées et on les liste*/
     let list_of_checked = [];
+    let list_of_non_checked = [];
     let nb_checked = 0;
     for (let receiver of receivers){
         if (receiver.checked){
             nb_checked += 1;
             list_of_checked.push(receiver.id);
-        } 
+        } else {
+            list_of_non_checked.push(receiver.id);
+        }
     }
 
     /*on calcule le montant par personne*/
-    let individualAmount = event.target.value/nb_checked; 
-    return [individualAmount, list_of_checked];
+    //let individualAmount = event.target.value/nb_checked; 
+    let individualAmount = amount.value/nb_checked; 
+    return [individualAmount, list_of_checked,list_of_non_checked];
 }
 
-function attributeIndividualAmount(individualAmount, list_of_checked){
+function attributeIndividualAmount(individualAmount, list_of_checked,list_of_non_checked){
     for (let checked_receiver of list_of_checked){
-        document.body.querySelector(`.${checked_receiver}-amount`).innerHTML = `${individualAmount} eur`;
+        document.body.querySelector(`.${checked_receiver}-amount`).innerHTML = `${individualAmount.toFixed(2)} eur`;
     }
+    for (let non_checked_receiver of list_of_non_checked){
+        document.body.querySelector(`.${non_checked_receiver}-amount`).innerHTML = `0.00 eur`;
+    }
+}
+
+function NumberOfCheckedPeople(){
+    let nb_checked = 0; 
+    for (let receiver of receivers){
+        if (receiver.checked){
+            nb_checked += 1; 
+        }  
+    } 
+    return nb_checked;
 }
